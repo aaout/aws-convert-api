349f38750fd43447a29d06c4eb6001959aac460b
aaout <aaout8@gmail.com>
2025-05-11 16:44:19 +0900

    tutorial code

--- src/3.tool-use-based-agent-loop.py
+++ src/3.tool-use-based-agent-loop.py
@@ -3,6 +3,7 @@
 import boto3
 import json
 import math
+from pydantic import BaseModel
 
 
 class ToolError(Exception):
@@ -34,3 +35,31 @@ def call_bedrock(message_list, tool_list):
     )
 
     return response
+
+
+def get_tool_result(tool_use_block):
+    """AgentがtoolUseした際のロジックを実装する関数
+    予め用意していおいたtool_listの中から、呼び出されたtoolに応じて条件分岐
+
+    Args:
+        tool_use_block (_type_): _description_
+
+    Raises:
+        ToolError: _description_
+
+    Returns:
+        _type_: _description_
+    """
+    tool_use_name = tool_use_block["name"]
+
+    print(f"Using tool {tool_use_name}")
+
+    # Note: We're deliberately excluding tangent so something magical can happen
+    if tool_use_name == "cosine":
+        return math.cos(tool_use_block["input"]["x"])
+    elif tool_use_name == "sine":
+        return math.sin(tool_use_block["input"]["x"])
+    elif tool_use_name == "divide_numbers":
+        return tool_use_block["input"]["x"] / tool_use_block["input"]["y"]
+    else:
+        raise ToolError(f"Invalid function name: {tool_use_name}")
